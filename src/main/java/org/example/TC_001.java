5fa9ba52a78d5ae17788e82a7f4a1dc746824546
Minu <testminu1111@gmail.com>
2024-08-11 20:59:09 +0530

    1st commit @ 18.44

---
+++ src/main/java/org/example/TC_001.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class TC_001 {
+
+@Test
+
+    void m1(){
+
+        System.out.println("This is my First Java PGM");
+    }
+
+
+}
