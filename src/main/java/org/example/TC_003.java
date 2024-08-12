5c1441816b81e58fd75994721477079ec6a8362d
Minu <testminu1111@gmail.com>
2024-08-12 07:45:39 +0530

    3 rd Commit File @7.41 on 12/8/24

---
+++ src/main/java/org/example/TC_003.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.testng.annotations.Test;
+
+public class TC_003 {
+
+    @Test
+
+    void m1(){
+
+        System.out.println("This is my First Java PGM1");
+        System.out.println("This is my First Java PGM2");
+        System.out.println("This is my First Java PGM3");
+        System.out.println("This is my First Java PGM4");
+    }
+
+
+
+
+}
